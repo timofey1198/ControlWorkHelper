80d8b2a890a3da0ceef59f0c782a87519f92ac73
timofey1198 <timostar98@gmail.com>
2017-11-27 10:25:53 +0300

    Создан класс Person с, добавлены основные свойства класса

---
+++ ControlWorkHelper/Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControlWorkHelper
+{
+    class Person
+    {
+        public string Name { get; }
+        public string Surname { get; }
+        public string Email { get; }
+        public Commit LastCommit { get; }
+        public int PenaltyPoints { get; }
+
+
+
+    }
+}
