d65100c16ede24df5234b8d1b539a226376fb716
timofey1198 <timostar98@gmail.com>
2017-12-01 00:39:04 +0300

    Added class Helper

---
+++ ControlWorkHelper/Helper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControlWorkHelper
+{
+    class Helper
+    {
+        public static int IntInput(string welcome)
+        {
+            int n = 0;
+            Console.WriteLine(welcome);
+            while (!int.TryParse(Console.ReadLine(), out n))
+            {
+                Console.WriteLine("Ошибка. Повторите ввод:");
+            }
+            return n;
+        }
+    }
+}
