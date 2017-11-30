ea9ea2cb6b1d8fda8ce29ce78b7acabd68eda03c
DimaT1 <dymych99@gmail.com>
2017-11-30 14:45:24 +0300

    added CSVHelper.cs

---
+++ ControlWorkHelper/CSVHelper.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ControlWorkHelper
+{
+    class CSVHelper
+    {
+        public static List<List<string>> ReadAll(string filename)
+        {
+            List<List<string>> res = new List<List<string>>();
+
+            using (StreamReader reader = new StreamReader(filename))
+            {
+                while (!reader.EndOfStream)
+                {
+                    string line = reader.ReadLine();
+                    List<string> values = line.Split(';').ToList();
+                    res.Add(values.ToList());
+                }
+            }
+            return res;
+        }
+    }
+}
