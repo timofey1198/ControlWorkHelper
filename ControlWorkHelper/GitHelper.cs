25f0e88b2ff627e5539d051ec1e56afb58481681
timofey1198 <timostar98@gmail.com>
2017-11-30 18:20:59 +0300

    Добавлена перегрузка функции  Log с параметром directory

--- ControlWorkHelper/GitHelper.cs
+++ ControlWorkHelper/GitHelper.cs
@@ -55,18 +55,46 @@ namespace ControlWorkHelper
         }
 
 
-        public static void Clone(string repos)
+        /// <summary>
+        /// Получает строку с логами текущей git ветки
+        /// </summary>
+        /// <param name="directory"></param>
+        /// <returns>
+        /// Строка с логами
+        /// </returns>
+        public static string Log(string directory)
         {
             // Задаем параметры процесса
             ProcessStartInfo psi = new ProcessStartInfo();
             psi.FileName = "git";
-            psi.Arguments = string.Format("clone {0}", repos);
+            psi.Arguments = string.Format("log");
+            psi.RedirectStandardOutput = true;
+            psi.RedirectStandardInput = true;
+            psi.UseShellExecute = false;
+            psi.WorkingDirectory = directory;
+            // Создаем процесс
+            Process process = Process.Start(psi);
+            string _out = process.StandardOutput.ReadToEnd();
+            process.Close();
+            return _out;
+        }
+
+        public static void Clone(string repos, string directory)
+        {
+            // Задаем параметры процесса
+            ProcessStartInfo psi = new ProcessStartInfo();
+            psi.FileName = "git";
+            psi.Arguments = string.Format("clone {0} {1}", repos, directory);
             psi.RedirectStandardOutput = true;
             psi.RedirectStandardInput = true;
             psi.UseShellExecute = false;
             // Создаем процесс
             Process process = Process.Start(psi);
             StreamReader output = process.StandardOutput;
+            while (!output.EndOfStream)
+            {
+                Console.WriteLine(output.ReadLine());
+            }
         }
     }
 }
