c7419f3d2cd9504de072bf75f8881b66198241fb
timofey1198 <timostar98@gmail.com>
2017-11-30 18:19:38 +0300

    Добавлены перегрузки методов GetAllCommits и ParseLog с параметром directory

--- ControlWorkHelper/LogParser.cs
+++ ControlWorkHelper/LogParser.cs
@@ -33,13 +33,43 @@ namespace ControlWorkHelper
                     commits.Add(new Commit(strDate, email, userName));
                 }
             }
+            return commits;
+        }
 
+        public static List<Commit> GetAllCommits(string directory)
+        {
+            string[] strCommits = ParseLog(directory);
+            int len = strCommits.Length;
+            //Commit[] commits = new Commit[len];
+            List<Commit> commits = new List<Commit>();
+
+            for (int i = 0; i < len; i++)
+            {
+                string commit = strCommits[i];
+                if (commit.Length > 40)
+                {
+                    string[] xxx = commit.Split();
+                    string userName = xxx[0];
+                    string email = xxx[1].Substring(1, xxx[1].Length - 2);
+                    Regex regex = new Regex(datePattern);
+                    Match match = regex.Match(commit);
+                    string strDate = match.Groups[0].Value;
+                    commits.Add(new Commit(strDate, email, userName));
+                }
+            }
             return commits;
         }
 
         private static string[] ParseLog()
         {
             string log = GitHelper.Log();
+            string[] Commits = Regex.Split(log, commitPattern);
+            return Commits;
+        }
+
+        private static string[] ParseLog(string directory)
+        {
+            string log = GitHelper.Log(directory);
             string[] Commits = Regex.Split(log, commitPattern);
             return Commits;
         }
